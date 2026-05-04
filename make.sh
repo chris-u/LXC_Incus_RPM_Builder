a6dd47a3fafc000cb8d5b221888e89fd88616ff1
Chris Ulrich <cdu-git@f82.us>
2026-05-04 08:33:28 -0400

    add nebula to build; support arm and intel

--- make.sh
+++ make.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
 
-incus_version=6.23.0
-lxc_version=6.0.6
+prefix=trussio
 
-sed -e s/{{INCUS_VERSION}}/$incus_version/g -e s/{{LXC_VERSION}}/$lxc_version/g < Dockerfile.proto > Dockerfile
+incus_version=7.0.0
+lxc_version=7.0.0
+nebula_version=1.10.3
+
+#incus_version=6.23.0
+#lxc_version=6.0.6
+#nebula_version=1.10.3
+
+#nebula_arch=arm64
+#build_arch=arm-native
+
+nebula_arch=amd64
+build_arch=x86-compat
+
+docker context use colima-$build_arch
+
+sed -e s/{{INCUS_VERSION}}/$incus_version/g \
+    -e s/{{LXC_VERSION}}/$lxc_version/g \
+    -e s/{{NEBULA_ARCH}}/$nebula_arch/g \
+    -e s/{{PREFIX}}/$prefix/g \
+    -e s/{{NEBULA_VERSION}}/$nebula_version/g  < Dockerfile.proto > Dockerfile
 
 docker build -t uek10-incus:lxc${lxc_version}_inc${incus_version} .
 
